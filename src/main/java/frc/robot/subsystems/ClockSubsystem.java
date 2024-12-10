3d7b5efd440e6a84d279e100ab97d1ebdeee7e72
IberiaAloeVera <oskarziobrowski84@gmail.com>
2024-12-10 18:17:12 -0500

    the robot code!

---
+++ src/main/java/frc/robot/subsystems/ClockSubsystem.java
@@ -0,0 +1,61 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class ClockSubsystem extends SubsystemBase {
+  /** Creates a new ExampleSubsystem. */
+  TalonFX clockMotor;
+
+  public ClockSubsystem() {
+     clockMotor = new TalonFX(Constants.clockPort);
+
+  }
+
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public Command ClockCommand() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+  public void setMotorSpeed(double setSpeed) {
+    clockMotor.set(setSpeed);
+  }
+ //public void setClockPosition(double setPosition) {
+  //    clockMotor.setControl(new PositionVoltage(setPosition));
+ //}
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean ClockCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
